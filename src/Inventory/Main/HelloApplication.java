b03e587ca9300eea9debb8c8e9016ac7413c2463
Nirvik Sharma <sharmanirvik1@gmail.com>
2021-08-17 17:35:01 -0600

    Inventory Page :)

---
+++ src/Inventory/Main/HelloApplication.java
@@ -0,0 +1,14 @@
+package Inventory.Main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloApplication {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
