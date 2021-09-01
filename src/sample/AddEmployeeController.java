311c4e4632077a9d7fcf96e9f3ce1f194cdef348
satanscorner <adeniro@student.neumont.edu>
2021-09-01 15:11:00 -0600

    Checks if input is valid

--- src/sample/AddEmployeeController.java
+++ src/sample/AddEmployeeController.java
@@ -3,12 +3,17 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddEmployeeController extends Controller implements Initializable {
     @FXML private TextField empName;
@@ -21,6 +26,28 @@ public class AddEmployeeController extends Controller implements Initializable {
     @FXML private TextField fridayShiftFX;
     @FXML private TextField saturdayShiftFX;
     @FXML private TextField sundayShiftFX;
+
+    private static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    Alert a = new Alert(Alert.AlertType.NONE);
+
+    private boolean isInputValid(TextField textField) {
+        Boolean isValid = false;
+    Pattern p = Pattern.compile("^(\\d{1,2})(:)(\\d{2})(-)(\\d{1,2})(:)(\\d{2})");
+    Matcher m = p.matcher(textField.getText());
+
+    while (m.find() || m.equals(null)) {
+        System.out.println("full match: " + m.group(0));
+        isValid = true;
+    }
+    return isValid;
+    }
 
     @FXML
     void addEmployeeToSchedule(ActionEvent event) {
@@ -37,16 +64,27 @@ public class AddEmployeeController extends Controller implements Initializable {
         s.read();
         if(index == -1){
             try {
-                System.out.println("line 40 " + empName.getText() + " " + mondayShiftFX.getText() + " " +
+                //boolean nameisInputValide = empName.getText()
+                boolean mondayisInputValid = isInputValid(mondayShiftFX);
+                boolean tuesdayisInputValid = isInputValid(tuesdayShiftFX);
+                boolean wednesdayisInputValid = isInputValid(wednesdayShiftFX);
+                boolean thursdayisInputValid = isInputValid(thursdayShiftFX);
+                boolean fridayisInputValid = isInputValid(fridayShiftFX);
+                boolean saturdayisInputValid = isInputValid(saturdayShiftFX);
+                boolean sundayisInputValid = isInputValid(sundayShiftFX);
+
+                if(mondayisInputValid || tuesdayisInputValid || wednesdayisInputValid || thursdayisInputValid ||
+                        fridayisInputValid || saturdayisInputValid || sundayisInputValid != false) {
+                    System.out.println("line 40 " + empName.getText() + " " + mondayShiftFX.getText() + " " +
                                 tuesdayShiftFX.getText() + " " +
                                 wednesdayShiftFX.getText() + " " +  thursdayShiftFX.getText() + " " +  fridayShiftFX.getText() + " " +
                         saturdayShiftFX.getText() + " " +  sundayShiftFX.getText());
                 super.s.AddEmployee(empName.getText(), mondayShiftFX.getText(), tuesdayShiftFX.getText(),
                         wednesdayShiftFX.getText(), thursdayShiftFX.getText(), fridayShiftFX.getText(),
                         saturdayShiftFX.getText(), sundayShiftFX.getText());
-//                super.s.AddEmployee(empName.getText(), mondayShiftFX.getText(), tuesdayShiftFX.getText(),
-//                        wednesdayShiftFX.getText(), thursdayShiftFX.getText(), fridayShiftFX.getText(),
-//                        saturdayShiftFX.getText(), sundayShiftFX.getText());
+                }else {
+                    showAlert(Alert.AlertType.ERROR, "Form Error!", "Please enter fields in correct form.");
+                }
                 System.out.println();
                 s.write();
             }catch (Exception e) {
